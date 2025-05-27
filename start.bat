ed5977a440fb56da9d01f4a617a530bc927746ba
bfilipovic523it <bfilipovic523it@raf.rs>
2025-05-27 19:16:40 +0200

    startbat

--- start.bat
+++ start.bat
@@ -1,9 +1,7 @@
 @echo off
     copy .env.example .env
     composer install
-    php artisan key:generate
     php artisan migrate --seed
-    npm install
-    start /B npm run dev
+    php artisan key:generate
     php artisan serve
 pause
