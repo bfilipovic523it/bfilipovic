df2e06e414db0ff7b87028845612d56b28f2b2e3
bfilipovic523it <bfilipovic523it@raf.rs>
2025-05-27 19:10:53 +0200

    startbat

--- start.bat
+++ start.bat
@@ -1,44 +1,9 @@
-@echo off 
-
-title Pokretanje Laravel Projekta
-color 0a
-echo.
-echo #################################################
-echo #      BOGDAN FILIPOVIC IT 5/23 PWA PROJEKAT    #
-echo #################################################
-echo.
-
-echo Proveravam Composer...
-where composer >nul 2>&1
-if %errorlevel% neq 0 (
-    echo ERROR: Composer nije instaliran!
-    echo Preuzmite sa: https://getcomposer.org/download/
-    pause
-    exit /b
-)
-
-echo Instaliram PHP zavisnosti...
-composer install
-
-echo Instaliram frontend zavisnosti...
-npm install
-
-echo Proveravam .env fajl...
-if not exist ".env" (
-    echo Kreiranje .env fajla...
+@echo off
     copy .env.example .env
-    echo .env fajl kreiran iz .env.example
-) else (
-    echo .env fajl vec postoji
-)
-
-echo Pokrecem migracije...
-php artisan migrate --seed
-
-echo Generisanje aplikativnog kljuca...
-php artisan key:generate
-
-start cmd /k "php artisan serve"
-start cmd /k "npm run dev"
-
+    composer install
+    php artisan key:generate
+    php artisan migrate --seed
+    npm install
+    start /B npm run dev
+    php artisan serve
 pause
